7cc4b9deb5658732394298118f4ded59cac72a6a
rauxw <hardikraut909@gmail.com>
2025-05-01 00:10:22 +0530

    Setup Mongo

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    const mongoConnect = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`Connected:${mongoConnect.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,28 @@
-const express = require("express");
-const app = express();
+import dotenv from "dotenv";
 
-app.listen();
+dotenv.config();
+
+import express from "express";
+import { connectDB } from "./config/db.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello</h1>");
+});
+
+async function main() {
+  try {
+    connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(`${error}`);
+  }
+}
+
+main();
