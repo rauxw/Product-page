37fb986fc5d2b057a044ed145a16096c5b9a191a
rauxw <hardikraut909@gmail.com>
2025-05-03 19:53:39 +0530

    hosting update

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,24 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRoute from "./routes/product.route.js";
 
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
+const __dirname = path.resolve();
 
+app.use(express.json());
 app.use("/apiv1/product", productRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 async function main() {
   try {
